e461a87151e6d6df05cb75a1a9c30e29b402b507
xixiaxixi <nobugexception@qq.com>
2019-04-28 23:27:44 +0800

    have u test your code?

--- center/app/routes/expertInfo.js
+++ center/app/routes/expertInfo.js
@@ -7,7 +7,7 @@ let con=null;
 router.get('/:id', function (req, res, next) {
     if(con==null)con=new Connection();//这么写是因为这个脚本初始化的时候隔壁mysql容器还没启动好
     let id = req.params.id;
-    let SQL = 'SELECT 创建时间 as `time`, 试验名称 as `name`,试验数据格式 as `format`, 试验描述 as `description`, 已结束 as `finished` FROM 试验 WHERE ID=? LIMIT 1';
+    let SQL = 'SELECT ID as `id`, 创建时间 as `time`, 试验名称 as `name`,试验数据格式 as `format`, 试验描述 as `description`, 已结束 as `finished` FROM 试验 WHERE ID=? LIMIT 1';
     con.mysql.query(SQL, [id], function (error, results, fields) {
         if (error) throw error;
         if(results.length<1){
