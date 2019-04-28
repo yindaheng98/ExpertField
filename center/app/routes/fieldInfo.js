06d15b8ba62a2071437d0cb07403b8e37ed40749
xixiaxixi <nobugexception@qq.com>
2019-04-28 14:54:23 +0800

    modify

--- center/app/routes/fieldInfo.js
+++ center/app/routes/fieldInfo.js
@@ -7,7 +7,7 @@ let con=null;
 router.get('/:id', function (req, res, next) {
     if(con==null)con=new Connection();//这么写是因为这个脚本初始化的时候隔壁mysql容器还没启动好
     let id = req.params.id;
-    let SQL = 'SELECT 创建时间 as `time`, 试验田名称 as `name`, 试验田描述 as `description` WHERE ID=? FROM 试验田';
+    let SQL = 'SELECT 创建时间 as `time`, 试验田名称 as `name`, 试验田描述 as `description` FROM 试验田 WHERE ID=?';
     con.mysql.query(SQL, [id], function (error, results, fields) {
         if (error) throw error;
         if(results.length<1){
